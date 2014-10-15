61af50c02ea3a1598144058e1bbabea1531cae90
aso1201754 <1201754@st.asojuku.ac.jp>
2014-10-15 13:04:52 +0900

    10/15

--- src/jp/ac/asojuku/route_en/Toko_Sentaku.java
+++ src/jp/ac/asojuku/route_en/Toko_Sentaku.java
@@ -1,5 +1,53 @@
 package jp.ac.asojuku.route_en;
 
-public class Toko_Sentaku {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class Toko_Sentaku extends Activity implements View.OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.toko_sentaku);
+
+		// button1登録
+		Button button1 = (Button)findViewById(R.id.button1);
+		button1.setOnClickListener(this);
+
+		// 配列準備（仮）
+		String[] members = {"ルート1", "ルート2"};
+		// listView1登録
+		ListView lv1 = (ListView)findViewById(R.id.listView1);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, members);
+		lv1.setAdapter(adapter);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		switch(v.getId()) {
+		// button1
+		case R.id.button1:
+			// MainActivityに遷移
+			Intent intent = new Intent(Toko_Sentaku.this, MainActivity.class);
+			startActivity(intent);
+
+		}
+
+	}
+
 
 }
